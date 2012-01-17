5f1b951c31ab0bbb6df647c945adda90c37af0cb
Mark Hamstra <hello@markhamstra.com>
2012-01-17 06:14:51 +0100

    Auto-ban (rather: add to Rampart's ban list) when hack attempts are found. Fixes #5  Evo exploit reflect_base is included, as are various spellings/alternations of phpmyadmin.
     Note that this depends on a bugfix not yet (at time of committing) in the Rampart source, and to actually prevent access to the site it needs the new feature I've written and submitted for approval in Rampart in Splittingred/Rampart#3 (https://github.com/splittingred/Rampart/pull/3).

--- core/components/notify404/elements/chunks/notifytpl.chunk.tpl
+++ core/components/notify404/elements/chunks/notifytpl.chunk.tpl
@@ -1,4 +1,5 @@
-<p>This is an automated email notification of a 404 error that has occured on the [[++site_name]] website.</p>
+[[+banned:notempty=`<p><strong>Notify404 has identified a common (but luckily failed) hack attempt in this request, and has added the available information to the Rampart ban list.</strong> Depending on you Rampart configuration the visitor may or may not still see the website.</p>`:default=``]]
+<p>This is an automated email notification of a 404 error that has occurred on the [[++site_name]] website.</p>
 <p>
     <strong>Date & time</strong>: [[+timestamp]] <br />
     <strong>Requested</strong>: [[+request]] <br />
@@ -8,8 +9,9 @@
     <strong>User agent</strong>: [[+user_agent]]
 </p>
 <p>
-    Automatic notifications like these can be disabled by disabling the Notify404 plugin in your [[++site_name]] MODX Manager.
+    Automatic notifications like these can be disabled by disabling the Notify404 plugin in your [[++site_name]] MODX
+    Manager.
 </p>
 <p style="font-style: italic;">
-    Powered by Notify404, developed by <a href="http://www.markhamstra.com/l/n404">Mark Hamstra</a>.
+    Powered by Notify404, developed by <a href="http://www.markhamstra.com/l/n404/">Mark Hamstra</a>
 </p>
--- core/components/notify404/elements/plugins/notify404.plugin.php
+++ core/components/notify404/elements/plugins/notify404.plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Notify404
- *
- * Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
+* Notify404
+*
+* Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
  *
  * This plugin is part of Notify404, an email notification utility for 404 errors.
  *
@@ -36,20 +36,58 @@ if (empty($mailto)) {
     return;
 }
 
+/* Auto-ban using RAMPART when requesting a known hack */
+$banned = false;
+$knownhacks = array(
+    'assets/snippets/reflect/snippet.reflect.php?reflect_base=', 'muieblackcat',
+    'admin/pma/index.php', 'admin/phpmyadmin/index.php', 'dbadmin/index.php', 'myadmin/index.php', 'mysql/index.php',
+    'mysqladmin/index.php', 'typo3/phpmyadmin/index.php', 'phpadmin/index.php', 'phpmyadmin1/index.php', 'phpmyadmin2/index.php',
+    'xampp/phpmyadmin/index.php', 'websql/index.php', 'phpmyadmin/index.php',
+);
+foreach ($knownhacks as $needle) {
+    if (stripos($_SERVER['REQUEST_URI'],$needle)) {
+        $block = true;
+
+        $modelPath = $modx->getOption('rampart.core_path',null,$modx->getOption('core_path').'components/rampart/').'model/';
+        $rampart = $modx->getService('rampart','Rampart',$modelPath.'rampart/');
+        if ($rampart instanceof Rampart) {
+            /* @var Rampart $rampart */
+            $ban = array(
+                Rampart::STATUS => Rampart::STATUS_BANNED,
+                Rampart::REASON => '',
+                Rampart::IP => $_SERVER['REMOTE_ADDR'],
+                Rampart::HOSTNAME => gethostbyaddr($_SERVER['REMOTE_ADDR']),
+                Rampart::EMAIL => '',
+                Rampart::USERNAME => '',
+                Rampart::USER_AGENT => $_SERVER['HTTP_USER_AGENT'],
+                Rampart::EXPIRATION	=> 30, //days
+                Rampart::REASON => 'Hack attempt found by Notify404. Known hack attempt '.$needle.' in request '. $_SERVER['REQUEST_URI'],
+                Rampart::SERVICE => 'Notify404',
+            );
+            
+            /* Rampart uses $modx->resource->get('id'), so we need to prevent that from breaking */
+            $modx->resource = $modx->getObject('modResource',$modx->getOption('error_page'));
+            if ($rampart->addBan($ban)) {
+                $banned = true;
+            }
+        }
+        break;
+    }
+}
 /* Prepare the data */
 $phs = array();
 $phs['ip'] = $_SERVER['REMOTE_ADDR'];
-$phs['host'] = 'http'. ($_SERVER['HTTPS'] ? 's' : null) .'://'. $_SERVER['HTTP_HOST'];
+$phs['host'] = 'http' . ($_SERVER['HTTPS'] ? 's' : null) . '://' . $_SERVER['HTTP_HOST'];
 $phs['request'] = $_SERVER['REQUEST_URI'];
 $phs['timestamp'] = date('d M Y - G:i:s');
 $phs['referer'] = ($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Not available.';
 $phs['user_agent'] = ($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Not available.';
+$phs['banned'] = $banned;
 
 /* Set the message */
 $message = $modx->getChunk($mailchunk,$phs);
 if (empty($message)) {
     $modx->log(modX::LOG_LEVEL_ERROR,'[Notify404] Error: empty message. Most likely the Notify404 template chunk does not exist or hasn\'t been set.');
-    $message = 'A 404 error was encountered on the site, however Notify404 isn\'t configured properly and cannot provide you properly formatted data. <br /> '.print_r($phs,true);
 }
 
 /* Send the email */
@@ -58,7 +96,7 @@ $modx->mail->set(modMail::MAIL_BODY,$message);
 $modx->mail->set(modMail::MAIL_FROM,$mailfrom);
 $modx->mail->set(modMail::MAIL_FROM_NAME,'Notify404');
 $modx->mail->set(modMail::MAIL_SENDER,'Notify404');
-$modx->mail->set(modMail::MAIL_SUBJECT,'[Notify404] Not found: '.$phs['request']);
+$modx->mail->set(modMail::MAIL_SUBJECT,'[Notify404]'. (($banned) ? ' [Banned]' : '') .' Not found: '.$phs['request']);
 $modx->mail->address('to',$mailto);
 $modx->mail->address('reply-to',$replyto);
 $modx->mail->setHTML(true);
@@ -66,6 +104,4 @@ if (!$modx->mail->send()) {
     $modx->log(modX::LOG_LEVEL_ERROR,'[Notify404] An error occurred while trying to send a 404 Page Not Found notification email.');
 }
 $modx->mail->reset();
-return '';
-
-?>
+return;
